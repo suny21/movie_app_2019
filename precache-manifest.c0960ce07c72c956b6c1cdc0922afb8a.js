23ec470087af37fb691fdc420d971ff7ad2c8b65
suny21 <suny2187@gmail.com>
2020-01-09 16:13:53 +0900

    Updates

---
+++ precache-manifest.c0960ce07c72c956b6c1cdc0922afb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de0d0e7b67c1253a7e091851c0b7760",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "dac7c37439e771aba0e8",
+    "url": "/movie_app_2019/static/css/main.53e9d22a.chunk.css"
+  },
+  {
+    "revision": "8526568d949b0ffebe66",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.a187f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dac7c37439e771aba0e8",
+    "url": "/movie_app_2019/static/js/main.086687cc.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
